4ad34a4f52aded9441f7865e1cf696e15a991346
Mahdis <57428228+mahdis1360@users.noreply.github.com>
2020-12-29 19:06:41 -0500

    Update UnitTest1.cs

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -11,48 +11,49 @@ namespace UnitTest
     [TestClass]
     public class UnitTest1
     {
-     //   [TestMethod]
-     
-      
-            //public void Test_Create_Method()
-            //{
-            //    //arrange
-            //    UserDto user = new UserDto()
-            //    {
-            //        Id = Guid.NewGuid(),
-            //        Name = "Joe",
-            //        Family="Smith",
-            //        Email="joesmith@outlook.com",
-            //        Password="1234",
-            //        UserName="joes"
-
-            //    };
-
-            //    Mock<IDataRepository<User>> mock = new Mock<IDataRepository<User>>();
-            //    UserLogic logic = new UserLogic(mock.Object);
-
-            //    try
-            //    {
-            //        logic.Add(user);
-
-            //    }
-            //    catch (Exception)
-            //    {
-
-            //        Assert.Fail("bad happen");
-            //    }
+        [TestMethod]
 
 
-            //}
+        public void Test_Create_Method()
+        {
+            //arrange
+            UserDto user = new UserDto()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Joe",
+                Family = "Smith",
+                Email = "joesmith@outlook.com",
+                Password = "1234",
+                UserName = "joes"
 
-            [TestMethod]
+            };
+            
+            Mock<IDataRepository<User>> mock = new Mock<IDataRepository<User>>();
+            UserLogic logic = new UserLogic(mock.Object);
+            UserDto[] users=null;
+            users[0] = user; 
+            try
+            {
+                logic.Add(users);
+
+            }
+            catch (Exception)
+            {
+
+                Assert.Fail("bad happen");
+            }
+
+
+        }
+
+        [TestMethod]
             public void Test_Get_Method()
             {
                 //arrange
                 Guid id = Guid.NewGuid();
-                Mock<IDataRepository<UserDto>> mock = new Mock<IDataRepository<UserDto>>();
+                Mock<IDataRepository<User>> mock = new Mock<IDataRepository<User>>();
                 mock.Setup(repo => repo.Get( i=> i.Id ==id)).Returns(
-                new UserDto()
+                new User()
                 {
                     Name = "Joe",
                     Family = "Smith",
@@ -72,69 +73,45 @@ namespace UnitTest
                 //assert
 
                 Assert.IsNotNull(logic);
-                Assert.AreEqual("Family", poco.Family);
 
+
+
+        }
+
+        [TestMethod]
+        public void Test_Update_Method()
+        {
+            //arrange
+
+            Mock<IDataRepository<User>> mock = new Mock<IDataRepository<User>>();
+            UserDto user = new UserDto()
+            {
+                Name = "Joe",
+                Family = "Smith",
+                Email = "joesmith@outlook.com",
+                Password = "1234",
+                UserName = "joes",
+                Id = Guid.NewGuid(),
+            };
+
+            UserLogic logic = new UserLogic(mock.Object);
+            UserDto[] users = null;
+            users[0] = user;
+
+            try
+            {
+                logic.Update(users);
 
             }
+            catch (Exception)
+            {
 
-        //    [TestMethod]
-        //    public void Test_Update_Method()
-        //    {
-        //        //arrange
-
-        //        Mock<IDataRepository<UserDto>> mock = new Mock<IDataRepository<UserDto>>();
-        //        UserDto user = new UserDto()
-        //        {
-        //            Name = "Joe",
-        //            Family = "Smith",
-        //            Email = "joesmith@outlook.com",
-        //            Password = "1234",
-        //            UserName = "joes",
-        //            Id = Guid.NewGuid(),
-        //        };
-
-        //        UserLogic logic = new UserLogic(null);
-
-        //        try
-        //        {
-        //            logic.Update(user);
-
-        //        }
-        //        catch (Exception)
-        //        {
-
-        //            Assert.Fail("bad happen");
-        //        }
-        //    }
-        //    [TestMethod]
-        //    public void Test_Search_Method()
-        //    {
-        //        //arrange
-        //        string searchdetail = "travel to Mexico";
-        //        Mock<IDataRepository<BookPoco>> mock = new Mock<IDataRepository<BookPoco>>();
-        //        mock.Setup(repo => repo.GetList(searchdetail)).Returns(
-        //        new BookPoco()
-        //        {
-        //            AutorId = Guid.NewGuid(),
-        //            Description = "travel to Mexico",
-        //            Title = "travel",
-        //            Price = 20,
-        //            Id = Guid.NewGuid(),
+                Assert.Fail("bad happen");
+            }
+        }
 
 
-        //        });
 
 
-        //        BookLogic logic = new BookLogic(mock.Object);
-        //        //act
-        //        BookPoco poco = logic.Search(searchdetail);
-        //        //assert
-
-        //        Assert.IsNotNull(logic);
-        //        Assert.AreEqual("travel to Mexico", poco.Description);
-
-
-        //    }
-        //}
     }
 }
